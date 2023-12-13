edecaed36a77c02c75886a09a386d34f44ef0fc9
Jay Pokale <jay.pokale.35@gmail.com>
2023-12-13 22:30:18 +0530

    JSDoc updated in main.js

--- main.js
+++ main.js
@@ -9,7 +9,26 @@ const Float64 = require("./lib/float/byte8");
 const BigInt64 = require("./lib/bigint/byte8");
 const BigUint64 = require("./lib/biguint/byte8");
 
-var TypedArray = function (type = "int32", length = 0) {
+/**
+ * @typedef {"int8" | "int16" | "int32" | "uint8" | "uint16" | "uint32" | "float32" | "float64" | "int64" | "uint64"} NumericArrayType
+ */
+
+/**
+ * Creates instances of different numeric array types based on the specified type and length.
+ *
+ * @param {NumericArrayType} [type="int32"] - The type of the array. Possible values: "int8", "int16", "int32", "uint8", "uint16", "uint32", "float32", "float64", "int64", "uint64".
+ * @param {number} [length=0] - The length of the array. Must be a non-negative integer.
+ * @returns {object} - An instance of the specified numeric array type.
+ * @throws {Error} - Throws an error if the array length is not valid or the type is not recognized.
+ *
+ * @example
+ * // Create a 32-bit integer array with a length of 10
+ * const intArray = NumArray("int32", 10);
+ *
+ * // Create a default 32-bit integer array (type defaults to "int32", length defaults to 0)
+ * const defaultArray = NumArray();
+ */
+var NumArray = function (type = "int32", length = 0) {
   if (length < 0) {
     throw new Error("Array length is not valid");
   }
@@ -31,7 +50,12 @@ var TypedArray = function (type = "int32", length = 0) {
   // BigInt
   if (type === "int64") return new BigInt64(length);
   if (type === "uint64") return new BigUint64(length);
+
   throw Error("Enter Valid Type");
 };
 
-module.exports = TypedArray;
+/**
+ * Module exports the NumArray function.
+ * @module NumArray
+ */
+module.exports = NumArray;
